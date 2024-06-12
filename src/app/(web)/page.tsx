dc9731a36a0d5b386dc5d06a13b9024b8013a62c
Isaiah <isaiahemails@gmail.com>
2024-06-12 17:08:16 +1200

    picture changed

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,9 +1,9 @@
-import ContentGrid from '@/components/content-grid'
-import markdownToHtml from '@/lib/markdownToHtml'
-import { load } from 'outstatic/server'
+import ContentGrid from "@/components/content-grid";
+import markdownToHtml from "@/lib/markdownToHtml";
+import { load } from "outstatic/server";
 
 export default async function Index() {
-  const { content, allPosts, otherCollections } = await getData()
+  const { content, allPosts, otherCollections } = await getData();
 
   return (
     <>
@@ -24,7 +24,7 @@ export default async function Index() {
           />
         )}
         {Object.keys(otherCollections).map((collection) => {
-          if (!collection.length) return null
+          if (!collection.length) return null;
           return (
             <ContentGrid
               key={collection}
@@ -33,75 +33,75 @@ export default async function Index() {
               collection={collection}
               viewAll
             />
-          )
+          );
         })}
       </div>
     </>
-  )
+  );
 }
 
 async function getData() {
-  const db = await load()
+  const db = await load();
 
   // get content for the homepage
   const page = await db
-    .find({ collection: 'pages', slug: 'home' }, ['content'])
-    .first()
+    .find({ collection: "pages", slug: "home" }, ["content"])
+    .first();
 
   // convert markdown to html
-  const content = await markdownToHtml(page.content)
+  const content = await markdownToHtml(page.content);
 
   // get all posts. Example of fetching a specific collection
   const allPosts = await db
-    .find({ collection: 'posts', status: 'published' }, [
-      'title',
-      'publishedAt',
-      'slug',
-      'coverImage',
-      'description',
-      'tags'
+    .find({ collection: "posts", status: "published" }, [
+      "title",
+      "publishedAt",
+      "slug",
+      "coverImage",
+      "description",
+      "tags",
     ])
     .sort({ publishedAt: -1 })
     .limit(3)
-    .toArray()
+    .toArray();
 
   // get remaining collections
   const collections = await db
     .find(
       {
         // $nor is an operator that means "not or"
-        $nor: [{ collection: 'posts' }, { collection: 'pages' }],
-        status: 'published'
+        $nor: [{ collection: "posts" }, { collection: "pages" }],
+        status: "published",
       },
       [
-        'collection',
-        'title',
-        'publishedAt',
-        'slug',
-        'coverImage',
-        'description'
+        "collection",
+        "title",
+        "publishedAt",
+        "slug",
+        "coverImage",
+        "description",
       ]
     )
     .sort({ publishedAt: -1 })
     .limit(3)
-    .toArray()
+    .toArray();
 
   // group remaining collections by collection
   const otherCollections = collections.reduce<{
-    [key: string]: (typeof collections)[0][]
+    [key: string]: (typeof collections)[0][];
   }>((acc, item) => {
     if (!acc[item.collection]) {
-      acc[item.collection] = []
+      acc[item.collection] = [];
     }
 
-    acc[item.collection].push(item)
+    acc[item.collection].push(item);
 
-    return acc
-  }, {})
+    return acc;
+  }, {});
 
   return {
     content,
+    otherCollections,
     allPosts,
-    otherCollections
-  }
+  };
 }
