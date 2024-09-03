9d27825f1cdf93e72950f6f9380746eb4e2f5916
Isaiah <isaiahemails@gmail.com>
2024-09-04 07:30:47 +1200

    my not our

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -45,7 +45,7 @@ export default function Services() {
   return (
     <div className="max-w-6xl h-[max-content] mx-auto px-4 py-16 border-y-2 border-gray-500">
       <h2 className="text-3xl font-bold text-center mb-12 text-gray-900 dark:text-gray-100">
-        Our Services
+        Services
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
         {services.map((service, index) => (
