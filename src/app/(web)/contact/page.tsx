b7d0e3c45191c4206001386f12f878e1f3807f1c
Isaiah <isaiahemails@gmail.com>
2024-11-27 12:18:53 +1300

    moved suspense

--- src/app/(web)/contact/page.tsx
+++ src/app/(web)/contact/page.tsx
@@ -1,13 +1,16 @@
 import ContactForm from "@/components/contact";
+import { Suspense } from "react";
 
 export default function ContactPage() {
-  return (
-    <div className="relative max-w-6xl mx-auto px-5 animate-fade-in delay-1000 opacity-0 duration-500">
-      <article className="py-5 md:py-24">
-        <div className="max-w-2xl md:mx-auto">
-          <ContactForm />
-        </div>
-      </article>
-    </div>
-  );
+	return (
+		<div className="relative max-w-6xl mx-auto px-5 animate-fade-in delay-1000 opacity-0 duration-500">
+			<article className="py-5 md:py-24">
+				<div className="max-w-2xl md:mx-auto">
+					<Suspense>
+						<ContactForm />
+					</Suspense>
+				</div>
+			</article>
+		</div>
+	);
 }
--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -4,7 +4,6 @@ import React, { useState, useEffect } from "react";
 import emailjs from "@emailjs/browser";
 import { motion } from "framer-motion";
 import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
 
 export type FormData = {
 	name: string;
@@ -123,88 +122,86 @@ export default function ContactForm() {
 	};
 
 	return (
-		<Suspense>
-			<div className="relative max-w-6xl md:mx-auto md:px-5 md:py-10 animate-fade-in delay-1000 opacity-0 duration-500">
-				<main className="bg-white dark:bg-gray-900 p-8 rounded-lg shadow-md">
-					<h1 className="text-3xl font-bold text-gray-900 dark:text-white mb-6">
-						Get In Touch
-					</h1>
-					<form onSubmit={handleSubmit} className="space-y-6">
-						<input
-							type="text"
-							placeholder="Full Name"
-							value={state.name}
-							onChange={(e) =>
-								setState((prevState) => {
-									return {
-										...prevState,
-										name: e.target.value,
-									};
-								})
-							}
-							required
-							className="w-full p-4 border border-gray-300 dark:border-gray-700 rounded-lg bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-blue-500"
-						/>
-						<input
-							type="email"
-							placeholder="Email"
-							value={state.email}
-							onChange={(e) =>
-								setState((prevState) => {
-									return {
-										...prevState,
-										email: e.target.value,
-									};
-								})
-							}
-							required
-							className="w-full p-4 border border-gray-300 dark:border-gray-700 rounded-lg bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-blue-500"
-						/>
-						<input
-							type="text"
-							placeholder="Subject"
-							value={state.subject}
-							onChange={(e) =>
-								setState((prevState) => {
-									return {
-										...prevState,
-										subject: e.target.value,
-									};
-								})
-							}
-							required
-							className="w-full p-4 border border-gray-300 dark:border-gray-700 rounded-lg bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-blue-500"
-						/>
-						<textarea
-							placeholder="Message"
-							value={state.message}
-							onChange={(e) =>
-								setState((prevState) => {
-									return {
-										...prevState,
-										message: e.target.value,
-									};
-								})
-							}
-							required
-							className="w-full p-4 h-[12rem] border border-gray-300 dark:border-gray-700 rounded-lg bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-blue-500 resize-none"
-						></textarea>
-						<motion.button
-							whileHover={{ scale: 1.05 }}
-							whileTap={{ scale: 0.95 }}
-							type="submit"
-							className="w-full bg-blue-500 text-white  dark:text-gray-900 p-4 rounded-lg font-bold focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-						>
-							Submit
-						</motion.button>
-					</form>
-					{state.confirmation && (
-						<h2 className="mt-6 text-lg text-center text-gray-900 dark:text-white">
-							{state.confirmation}
-						</h2>
-					)}
-				</main>
-			</div>
-		</Suspense>
+		<div className="relative max-w-6xl md:mx-auto md:px-5 md:py-10 animate-fade-in delay-1000 opacity-0 duration-500">
+			<main className="bg-white dark:bg-gray-900 p-8 rounded-lg shadow-md">
+				<h1 className="text-3xl font-bold text-gray-900 dark:text-white mb-6">
+					Get In Touch
+				</h1>
+				<form onSubmit={handleSubmit} className="space-y-6">
+					<input
+						type="text"
+						placeholder="Full Name"
+						value={state.name}
+						onChange={(e) =>
+							setState((prevState) => {
+								return {
+									...prevState,
+									name: e.target.value,
+								};
+							})
+						}
+						required
+						className="w-full p-4 border border-gray-300 dark:border-gray-700 rounded-lg bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-blue-500"
+					/>
+					<input
+						type="email"
+						placeholder="Email"
+						value={state.email}
+						onChange={(e) =>
+							setState((prevState) => {
+								return {
+									...prevState,
+									email: e.target.value,
+								};
+							})
+						}
+						required
+						className="w-full p-4 border border-gray-300 dark:border-gray-700 rounded-lg bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-blue-500"
+					/>
+					<input
+						type="text"
+						placeholder="Subject"
+						value={state.subject}
+						onChange={(e) =>
+							setState((prevState) => {
+								return {
+									...prevState,
+									subject: e.target.value,
+								};
+							})
+						}
+						required
+						className="w-full p-4 border border-gray-300 dark:border-gray-700 rounded-lg bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-blue-500"
+					/>
+					<textarea
+						placeholder="Message"
+						value={state.message}
+						onChange={(e) =>
+							setState((prevState) => {
+								return {
+									...prevState,
+									message: e.target.value,
+								};
+							})
+						}
+						required
+						className="w-full p-4 h-[12rem] border border-gray-300 dark:border-gray-700 rounded-lg bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-blue-500 resize-none"
+					></textarea>
+					<motion.button
+						whileHover={{ scale: 1.05 }}
+						whileTap={{ scale: 0.95 }}
+						type="submit"
+						className="w-full bg-blue-500 text-white  dark:text-gray-900 p-4 rounded-lg font-bold focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+					>
+						Submit
+					</motion.button>
+				</form>
+				{state.confirmation && (
+					<h2 className="mt-6 text-lg text-center text-gray-900 dark:text-white">
+						{state.confirmation}
+					</h2>
+				)}
+			</main>
+		</div>
 	);
 }
